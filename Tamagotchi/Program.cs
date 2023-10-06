1288b60acec427e8bab6da609524954090ffe079
Namith Marasinha <namith.marasinhamudiyanselage@elev.ga.ntig.se>
2023-10-06 10:03:14 +0200

    initial

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,10 @@
+﻿using Namespace;
+
+Console.WriteLine("Welcome to the Tamagotchi");
+
+Tamagotchi tamy = new Tamagotchi();
+
+Console.WriteLine ("Choose a name for your Tamagotchi");
+
+tamy.name = Console.ReadLine(); 
+
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,13 @@
+namespace Namespace;
+public class Tamagotchi
+{
+    private int hunger; 
+    private int boredom;
+    private List<string> words =  new List<string>() {"Welcome"};
+    private bool isAlive; 
+    private Random generator; 
+
+    public string name; 
+
+}
+
