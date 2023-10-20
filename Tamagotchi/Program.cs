ab757051559800cd723f61cb7b5f181da101f990
Namith Marasinha <namith.marasinhamudiyanselage@elev.ga.ntig.se>
2023-10-20 10:47:16 +0200

    final

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,10 +1,52 @@
-﻿using Namespace;
+﻿using Microsoft.VisualBasic;
+using Namespace;
+
 
 Console.WriteLine("Welcome to the Tamagotchi");
 
 Tamagotchi tamy = new Tamagotchi();
 
-Console.WriteLine ("Choose a name for your Tamagotchi");
+Console.WriteLine("Choose a name for your Tamagotchi");
 
-tamy.name = Console.ReadLine(); 
+tamy.name = Console.ReadLine();
+
+Console.WriteLine($"{tamy.name} is a excellent name!");
+
+
+while (tamy.GetAlive() == true)
+{
+
+    Console.Clear();
+    tamy.PrintStats();
+    Console.WriteLine("What would you like to do with your pet?");
+    Console.WriteLine($" 1. make {tamy.name} say a word?");
+    Console.WriteLine($" 2. feed {tamy.name} some food?");
+    Console.WriteLine($" 3. make {tamy.name} speak with you?");
+    Console.WriteLine($" 4. do nothing.");
+
+    string option = Console.ReadLine();
+    if (option == "1")
+    {
+        Console.WriteLine($"Which word would you like {tamy.name} to say?");
+        string word = Console.ReadLine();
+        tamy.Teach(word);
+    }
+    if (option == "2")
+    {
+        tamy.Feed();
+    }
+    if (option == "3")
+    {
+        tamy.Hi();
+    }
+    else
+    {
+        Console.WriteLine("Does nothing...");
+    }
+    tamy.Tick();
+}
+
+Console.WriteLine("OH MY GOD HE/SHE IS DAED!!!!!!!");
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
 
--- Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi.cs
@@ -1,13 +1,75 @@
 namespace Namespace;
 public class Tamagotchi
 {
-    private int hunger; 
+    private int hunger;
     private int boredom;
-    private List<string> words =  new List<string>() {"Welcome"};
-    private bool isAlive; 
-    private Random generator; 
+    private List<string> words = new List<string>() { "Welcome" };
+    private bool isAlive;
+    private Random generator;
 
-    public string name; 
+    public string name;
+
+    public Tamagotchi()
+    {
+        isAlive = true;
+    }
+
+    public void Feed()
+    {
+        Console.WriteLine($"[{name}] eats and becomes less hungry");
+        hunger -= 2;
+        if (hunger < 0)
+        {
+            hunger = 0;
+        }
+    }
+
+
+    public void Hi()
+    {
+        int wordNum = generator.Next(words.Count);
+        Console.WriteLine($"[{name}] says: {words[wordNum]}");
+        ReduceBoredom();
+    }
+
+
+    public void Teach(string word) {
+        Console.WriteLine($"[{name}] learns: {word}");
+        words.Add(word);
+        ReduceBoredom();
+    }
+
+
+    public void Tick() {
+        hunger++;
+        boredom++;
+        if(hunger > 10 || boredom > 10) {
+            isAlive = false;
+        }
+    }
+
+    public void PrintStats() {
+        Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocubulary: {words.Count} words");
+    }
+
+    public bool GetAlive() {
+        return isAlive; 
+    }
+
+
+    public void ReduceBoredom() {
+        Console.WriteLine($"[{name}] is now less bored");
+
+        boredom -= 2;
+        if (boredom <0) {
+            boredom = 0;
+        }
+    }
+
+
 
 }
+
+
+
 
